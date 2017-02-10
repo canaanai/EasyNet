20bcc7900d2aef72c47c9f5b4568b6e3e4945c54
canaanai <canaanai@users.noreply.github.com>
2017-02-10 15:07:37 +0800

    dd

--- easynet2/src/androidTest/java/com/jaqen/easynet/ExampleInstrumentedTest.java
+++ easynet2/src/androidTest/java/com/jaqen/easynet/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.jaqen.easynet", appContext.getPackageName());
+        assertEquals("com.jaqen.easynet.test", appContext.getPackageName());
     }
 }
